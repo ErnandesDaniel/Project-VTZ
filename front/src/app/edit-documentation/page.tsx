003f69efba186ccf8842744716006799122f378c
Anna Faybushevskaya <avfaybushevskaya@gmail.com>
2024-12-02 20:45:27 +0300

    сформирована страничка Для Редактирования Документации (вернуться к цвету и форме кнопки Сохранить)

--- front/src/app/edit-documentation/page.tsx
+++ front/src/app/edit-documentation/page.tsx
@@ -4,58 +4,54 @@ import Page from "@/components/Page/Page";
 import Text from "@/components/Universal/Text/Text";
 import './edit-documentation.css';
 import { Input, Flex } from 'antd';
-const { Search } = Input;
-import { Checkbox } from "antd";
 import Button from "@/components/Universal/Button/Button";
 import CheckBoxButton from "@/app/edit-documentation/components/check-box-button";
 
-export default function create_VTZ() {
+export default function edit_documentation() {
 
-    const projectInstitutes=['СТО', 'СПбПИ', 'МПИ', 'НПИ','Общая практика'];
+    const typeDocumentation = ['ПД', 'РД', 'Анализы', 'ВАБ-1'];
 
-    return(<Page className='edit-documentation-page'>
-        <Spacer space={22} />
-        <Text className='title'>Редактирование документации</Text>
-        <Spacer space={30} />
-        <Flex gap={20}>
-            <Text className='text'>Номер ВТЗ</Text>
-            <Input
-                className='input-text'
-                placeholder='Введите номер ВТЗ'
-            />
-        </Flex>
-        <Spacer space={30} />
-        <Flex gap={20}>
-            <Text className='text'>Вид ВТЗ</Text>
-            <Input
-                className='input-text'
-                placeholder='Введите название ВТЗ'
-            />
-        </Flex>
-        <Spacer space={20} />
-        <Checkbox>Требует ли уточнения</Checkbox>
-        <Spacer space={20} />
-        <Flex gap={20}>
-            <Text className='text'>Разделы ПД/РД</Text>
-            <Search
-                className='input-text'
-                placeholder='Выберите разделы ПД/РД'
-            />
-            <Checkbox>Влияет на все разделы ПД/РД</Checkbox>
-        </Flex>
-        <Spacer space={30} />
-        <Flex gap={15}>
-            <Text className='text'>Проектные институты</Text>
-            {
-                projectInstitutes.map((text)=> {
-                    return <CheckBoxButton key={text} text={text}/>;
-                })
-            }
-        </Flex>
-        <Spacer space={30} />
-        <Flex gap={50}>
-            <Button  title='Сохранить'/>
-            <Button type='default'  title='Назад к списку'/>
-        </Flex>
-    </Page>);
+    return (
+        <Page className='edit-documentation-page'>
+            <Spacer space={22} />
+            <Text className='title'>Редактирование документации</Text>
+            <Spacer space={40} />
+
+            <div className="form-container">
+                {/* Вид документации */}
+                <div className="form-group">
+                    <Text className='label'>Вид документации</Text>
+                    <div className="checkbox-group">
+                        {typeDocumentation.map((text) => (
+                            <CheckBoxButton key={text} text={text} />
+                        ))}
+                    </div>
+                </div>
+
+                {/* Наименование */}
+                <div className="form-group">
+                    <Text className='label'>Наименование</Text>
+                    <Input
+                        className='input-text'
+                        placeholder='Введите наименование документа'
+                    />
+                </div>
+
+                {/* Описание */}
+                <div className="form-group">
+                    <Text className='label'>Описание</Text>
+                    <Input.TextArea
+                        className='textarea'
+                        placeholder='Описание документа'
+                        rows={4}
+                    />
+                </div>
+            </div>
+
+            <Spacer space={30} />
+            <div className="button-container">
+                <Button title='Сохранить'/>
+            </div>
+        </Page>
+    );
 }
