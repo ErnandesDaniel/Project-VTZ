b3b8d61d8e395c488c9e9e77abac4c945b7e2377
Ernandes <Ernandes20@yandex.ru>
2024-12-07 19:46:27 +0300

    Добавлено получение списка ВТЗ с бэка

--- front/src/app/vtz-table/page.tsx
+++ front/src/app/vtz-table/page.tsx
@@ -4,6 +4,9 @@ import Spacer from "@/components/Universal/Spacer/Spacer";
 import Page from "@/components/Page/Page";
 import {columns} from "@/app/vtz-table/table-columns";
 import {dataSource} from "@/app/vtz-table/table-mock-data";
+import {useEffect, useState} from "react"
+import axios from "axios"
+
 const VTZTable = () => {
     const tableParams = {
         pagination: {
@@ -11,13 +14,28 @@ const VTZTable = () => {
         },
     };
 
+    const [vtz, setVtz]=useState([]);
+
+    useEffect(()=>{
+        axios.get('http://localhost:6001/http://localhost:5000/api/TaskVTZ/GetAll?withData=true').then((res)=>{
+            setVtz(res.data.value.$values.map(({taskNumber, taskName, practices, sections})=>{
+                return{
+                VTZ_number:taskNumber,
+                VTZ_type:taskName,
+                project_institutes:practices.$values.map(({practiceShortName})=>practiceShortName),
+                documentation: sections.$values.map(({sectionName})=>sectionName),
+                }
+            }));
+        });
+    },[]);
+
     return (
         <Page>
             <Spacer space={22} />
             <div className="vtz-table-wrapper">
                 <Table
                     className="vtz-table"
-                    dataSource={dataSource}
+                    dataSource={vtz}
                     columns={columns}
                     pagination={tableParams.pagination}
                 />
