2b5bfd5b9e12880b44b336fe45be94c0c519b7c6
Ernandes <Ernandes20@yandex.ru>
2025-03-15 16:58:00 +0300

    Исправление ошибки отсутствия window объекта

--- front/src/utils/apiInstance.js
+++ front/src/utils/apiInstance.js
@@ -1,11 +1,16 @@
+'use client'
 import axios from 'axios';
+const apiInstance=(()=>{
 
-const apiInstance = axios.create({
-    baseURL: `${window.location.protocol}//${window.location.hostname}:5000/api`, // Replace with your API base URL
-    timeout: 3000,
-    headers: {
-        'Content-Type': 'application/json',
-    },
-});
+    const apiInstance = axios.create({
+        baseURL: `${window.location.protocol}//${window.location.hostname}:5000/api`, // Replace with your API base URL
+        timeout: 3000,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    return apiInstance;
+})();
 
 export default apiInstance;
